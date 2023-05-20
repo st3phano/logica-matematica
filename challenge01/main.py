9271a712742f6834681ee9a694c8626c2f5c73d1
st3phano <stephano-@hotmail.com>
2023-05-20 12:44:57 -0300

    improve description of performed actions using print()

--- challenge01/main.py
+++ challenge01/main.py
@@ -10,17 +10,26 @@ from replace_connectives import replaceBiconditional
 from replace_connectives import replaceConditional
 
 def main():
+   print("Logical connectives symbols:")
+   print(f"[-] not: {connectives.NOT}")
+   print(f"[-] and: {connectives.AND}")
+   print(f"[-] or: {connectives.OR}")
+   print(f"[-] conditional: {connectives.COND}")
+   print(f"[-] biconditional: {connectives.BICOND}")
    formula = input("Type the desired formula:\n")
    
    # remove all white-space from the string
    WHITE_SPACE_REGEX = r"\s+"
    formula = re.sub(WHITE_SPACE_REGEX, "", formula)
-   print(f"{formula}\n")
+   print(f"\nFormula without white-space:\n{formula}\n")
 
-   formula = replaceConnective(formula, connectives.BICOND, True, replaceBiconditional)
-   formula = replaceConnective(formula, connectives.COND, True, replaceConditional)
+   printSteps = True;
+   if (printSteps):
+      print("Steps:")
+   formula = replaceConnective(formula, connectives.BICOND, printSteps, replaceBiconditional)
+   formula = replaceConnective(formula, connectives.COND, printSteps, replaceConditional)
 
-   print(formula)
+   print(f"Formula with only NOT, AND and OR:\n{formula}")
 
 if __name__ == "__main__":
    main()
